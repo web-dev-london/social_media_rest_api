78eb2bf23e9e79cae507dcb3ae9d0f60f0029431
Murod Akhmedov <murod.london@gmail.com>
2024-11-23 18:29:01 +0000

    Create MdView component to read md file

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+import MdView from "@/components/MdView";
 
 export default function Home() {
   return (
     <>
-
+      <div className="max-w-3xl mx-auto my-14">
+        <MdView />
+      </div>
     </>
   );
 }
---
+++ components/MdView.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { promises as fs } from "fs";
+import path from "path";
+import rehypeHighlight from "rehype-highlight";
+import remarkGfm from "remark-gfm";
+import { compileMDX } from 'next-mdx-remote/rsc';
+
+const MdView = async () => {
+  // Load the markdown content
+  const content = await fs.readFile(
+    path.join(process.cwd(), 'md', 'endpoints.md'),
+    'utf-8'
+  );
+
+  // Compile MDX content with plugins for styling and highlighting
+  const { content: renderedContent } = await compileMDX({
+    source: content,
+    options: {
+      parseFrontmatter: true,
+      mdxOptions: {
+        remarkPlugins: [remarkGfm],         // Enable GitHub-flavored Markdown extensions
+        rehypePlugins: [rehypeHighlight],   // Syntax highlighting for code blocks
+      },
+    },
+    components: {
+      // Define custom components if needed (e.g., specific heading styles or code block customizations)
+    },
+  });
+
+  return (
+    <article
+      className="prose mx-auto
+        text-base md:text-[18px] leading-loose
+      "
+    >
+      {renderedContent}
+    </article>
+  );
+};
+
+export default MdView;
